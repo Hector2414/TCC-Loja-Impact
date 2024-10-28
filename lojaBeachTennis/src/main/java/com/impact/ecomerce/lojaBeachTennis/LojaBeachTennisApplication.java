fa03eb2b827f8e16ab1395a95a05ec6cee460a3c
Hector2414 <hectorlaucher6@gmail.com>
2024-10-28 10:42:29 -0300

    ...28-10-2024

---
+++ lojaBeachTennis/src/main/java/com/impact/ecomerce/lojaBeachTennis/LojaBeachTennisApplication.java
@@ -0,0 +1,13 @@
+package com.impact.ecomerce.lojaBeachTennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaBeachTennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaBeachTennisApplication.class, args);
+	}
+
+}
---
+++ lojaBeachTennis/src/test/java/com/impact/ecomerce/lojaBeachTennis/LojaBeachTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.ecomerce.lojaBeachTennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaBeachTennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
